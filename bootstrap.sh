b208e9bf402f7ffbcd5a5d60d278f67a299d88d3
Ben Oxley <ben.oxley@gmail.com>
2025-10-25 21:29:32 +0100

    Add installation comments to bootstrap.sh
    Added comments for installation steps.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,14 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
+# Install Frigate
+
+# Install postgres with timescaledb
+
+# Install grafana
+
+# Install homeassistant
+
+# Install pihole
+
+# Install portainer
+
