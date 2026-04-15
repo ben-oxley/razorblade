cb6a30f23dc3905a71a34999faa1239b1674cdf7
Ben Oxley <ben.oxley@gmail.com>
2026-04-15 21:36:28 +0100

    Add MQTT logs table creation to postgres setup

--- postgres-setup.sh
+++ postgres-setup.sh
@@ -1,1 +1,9 @@
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'password';"
+sudo -u postgres psql -d postgres <<EOF
+CREATE TABLE mqtt_logs (
+    id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP WITHOUT TIME ZONE,
+    topic TEXT,
+    message TEXT
+);
+EOF
